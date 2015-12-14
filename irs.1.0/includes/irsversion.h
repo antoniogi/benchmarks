df4552e20ba1acac2440d0392d33a951646739b8
antoniogi <antoniogi@gmail.com>
2015-12-14 09:49:43 -0600

    Added irs (MPI code)

---
+++ irs.1.0/includes/irsversion.h
@@ -0,0 +1,6 @@
+#define VER_NODENAME "staff.stampede.tacc.utexas.edu"
+#define VER_DATE "12/14/15"
+#define VER_TIME "09:45:14"
+#define VER_OS "Linux 2.6.32-431.17.1.el6.x86_64 #1 SMP Wed May 7 23:32:49 UTC 2014"
+#define VER_OPT "Debug"
+#define VER_PERSON "agomez"
